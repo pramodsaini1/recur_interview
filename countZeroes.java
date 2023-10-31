cc9c1995f3ab539149ebdd95bde61c3b92a4f9c5
pramodsaini <pramodsaini25062000@gmail.com>
2023-10-31 02:50:59 -0700

    this is count the zeroes in the number using the recursion

---
+++ countZeroes.java
@@ -0,0 +1,21 @@
+package recur_interview;
+
+public class countZeroes {
+  private static int helper(int n,int c){
+      if(n==0){
+        return c;
+      }
+      int rem=n%10;
+      if(rem==0){
+          return helper(n/10, c+1);
+      }
+      return helper(n/10, c);
+  }
+  static int  count(int n){
+       return helper(n,0);
+  }
+    public static void main(String []arr){
+       System.out.println(count(30204));
+
+    }
+}
---
+++ palinNumber.java
@@ -0,0 +1,18 @@
+package recur_interview;
+
+public class palinNumber {
+    static int rev(int n){
+        if(n==0){
+            return 0;
+        }
+        else{
+            
+        }
+    }
+      static boolean palin(int n){
+           return n==rev(n);
+      }
+    public static void main(String []arr){
+        System.out.println(palin(12321));
+    }
+}
