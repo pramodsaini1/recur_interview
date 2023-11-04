329954fa079e1f0c44a999d15a7e88f5fcc85de8
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-04 01:20:03 -0700

    this is print 12345 using the recursion

---
+++ Array/Pattern/pa.java
@@ -0,0 +1,14 @@
+package recur_interview.Array.Pattern;
+
+public class pa {
+    static void display(int i){
+        if(i<=5){
+            System.out.print(i+" ");
+            display(i+1);
+        }
+    }
+    public static void main(String[] args) {
+        int i=1;
+        display(i);
+    }
+}
