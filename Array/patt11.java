4099b3b6d117e33033f59bdf7ecd9bd4b0c92a5d
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-13 00:53:19 -0800

    this is the  print the pattern is  *     ***    *****   *******  *********  in the java

---
+++ Array/patt11.java
@@ -0,0 +1,25 @@
+package recur_interview.Array;
+
+public class patt11 {
+     public static void main(String[] args) {
+                for(int i=1;i<=5;i++){
+                    for(int j=5-i;j>=0;j--){
+                        System.out.print(" ");
+                                }
+                    for(int k=1;k<=2*i-1;k++){
+                        System.out.print("*");
+                    }
+                    System.out.println();
+               }
+     }
+}
+/*
+ 
+ *
+    ***
+   *****
+  *******
+ *********
+
+ 
+ */
