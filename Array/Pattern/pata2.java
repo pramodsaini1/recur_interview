51aab1c6ea543ac63b599e431b1829c9aa12225a
pramodsaini <pramodsaini25062000@gmail.com>
2023-11-22 00:00:45 -0800

    this is print the pattern is 1 1 1 1 1 1 2 2 2 2 2 3 3 3 3 4 4 4 5 5 6 in the java

---
+++ Array/Pattern/pata2.java
@@ -0,0 +1,12 @@
+package recur_interview.Array.Pattern;
+
+public class pata2 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=5-i;j>=0;j--){
+                System.out.print(" ");
+            }
+            for
+        }
+    }
+}
---
+++ Array/Pattern/pata3.java
@@ -0,0 +1,21 @@
+package recur_interview.Array.Pattern;
+
+public class pata3 {
+     public static void main(String[] args) {
+                for(int i=1;i<=6;i++){
+                    for(int j=7-i;j>=1;j--){
+                        System.out.print(i+" ");
+                    }
+                System.out.println();
+            }
+     }
+}
+/*
+1 1 1 1 1 1
+2 2 2 2 2
+3 3 3 3
+4 4 4
+5 5
+6
+
+ */
